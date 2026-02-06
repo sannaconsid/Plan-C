72a57383b926b01320feb95e975a6b7b43c279ef
sannaconsid <sanna.lundqvist@consid.se>
2026-02-06 12:49:44 +0100

    i18n

--- frontend-angular/ember/src/app/app.ts
+++ frontend-angular/ember/src/app/app.ts
@@ -1,16 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBar } from '../nav-bar/nav-bar';
+import { NavBarComponent } from '../nav-bar/nav-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, MatSidenavModule],
+  imports: [RouterOutlet, MatButtonModule, MatSidenavModule, NavBarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Ember');
+  
   
 }
--- frontend-angular/ember/src/nav-bar/nav-bar.ts
+++ frontend-angular/ember/src/nav-bar/nav-bar.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, RouterLink, MatListModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss',
 })
-export class NavBar {
+export class NavBarComponent {
 
+  protected readonly title = signal($localize`Ember`);
 }
